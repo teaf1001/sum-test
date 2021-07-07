08361b9aa826074e4cf07cf1442dc717d1301004
teaf1001 <teaf1001@naver.com>
2021-07-07 16:46:49 +0900

    modified sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -1,10 +1,8 @@
 //sum.cpp
-#include<stdio.h>
-#include "sum.h"
 
-int main(){
-	int s = sum(10);
-	printf("result=%d\n", s);
+int sum(int n){
+	int res = 0;
+	for ( int i = 1; i <= n; ++i)
+		res +=i;
+	return res;
 }
-
-
